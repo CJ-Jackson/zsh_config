568c3f020346a3e8da6f30f46a18d7b4736fbc6e
Chris Jackson <hello@cjjackson.dev>
2023-08-24 10:47:08 +0100

    Mac compatibility

--- style.zsh
+++ style.zsh
@@ -2,7 +2,7 @@ zstyle '*' single-ignored show
 
 zstyle ':completion:*' completer _extensions _complete _approximate
 zstyle ':completion:*' use-cache yes
-zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+zstyle ':completion:*' cache-path "$HOME/.cache/zsh/.zcompcache"
 zstyle ':completion:*:*:*:*:processes' command 'ps -u cjjackson -o pid,user,comm -w -w'
 zstyle ':completion:*:*:*:users' ignored-patterns adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm ldap lp mail mailman mailnull man messagebus mldonkey mysql nagios named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp usbmux uucp vcsa wwwrun xfs '_*'
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
