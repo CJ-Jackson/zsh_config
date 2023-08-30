24d5eee2c3c132ba15e578739c3a18a130d7570b
Chris Jackson <hello@cjjackson.dev>
2023-08-30 17:39:10 +0100

    Added more options

--- options.zsh
+++ options.zsh
@@ -1,6 +1,32 @@
-setopt auto_cd
-setopt hist_ignore_dups
+# Navigation
+
+setopt AUTO_CD
+
+setopt AUTO_PUSHD
+setopt PUSHD_IGNORE_DUPS
+setopt PUSHD_SILENT
+
+setopt CORRECT
+setopt CDABLE_VARS
+setopt EXTENDED_GLOB
+
+# History
+
+setopt EXTENDED_HISTORY
+setopt SHARE_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_FIND_NO_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_VERIFY
+
+# Completion
+
 setopt MENU_COMPLETE
+
+# Bind Key
 
 bindkey -v
 
