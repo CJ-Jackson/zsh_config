17b6bc0094165ca4d9789cdb67644657cf273868
Chris Jackson <hello@cjjackson.dev>
2023-09-11 20:48:28 +0100

    Use eza if avaliable

--- alias.zsh
+++ alias.zsh
@@ -1,5 +1,11 @@
-alias ls='ls --color=auto'
-alias la='ls -la --block-size=K'
+if command -v eza > /dev/null
+then
+	alias ls='eza'
+	alias la='eza -lh'
+else
+	alias ls='ls --color=auto'
+	alias la='ls -la --block-size=K'
+fi
 
 alias pbcopy='wl-copy'
 alias pbpaste='wl-paste'
