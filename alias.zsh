5891fc8de7bbdf98a75fbb0a6c5dd792e2b0d0d1
Chris Jackson <hello@cjjackson.dev>
2023-10-08 09:21:10 +0100

    Added la-t

--- alias.zsh
+++ alias.zsh
@@ -2,6 +2,7 @@ if command -v eza > /dev/null
 then
 	alias ls='eza --icons'
 	alias la='ls -lah'
+	alias la-t='la -t modified --sort oldest'
 else
 	alias ls='ls --color=auto'
 	alias la='ls -la --block-size=K'
